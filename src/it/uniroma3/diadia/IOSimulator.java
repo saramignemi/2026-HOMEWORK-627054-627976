bb0b62ec04852ffd9171057dcd7ac47230aede7c
Juan <eysless@gmail.com>
2026-06-10 01:32:53 +0200

    make iosimulator much better

--- src/it/uniroma3/diadia/IOSimulator.java
+++ src/it/uniroma3/diadia/IOSimulator.java
@@ -1,23 +1,38 @@
 package it.uniroma3.diadia;
 
-public class IOSimulator implements IO{
-	private String lastString;
-	private String[] inputs;
-	private int index;
-	
-	public IOSimulator(String... inputs) {
-		this.inputs = inputs;
-		this.index = 0;
-	}
-	
-	public String leggiRiga() {
-		return this.inputs[this.index++];
-	}
-	public void mostraMessaggio(String messaggio) {
-		this.lastString = messaggio;
-	}
-	
-	public String getLastString() {
-		return this.lastString;
-	}
+import java.util.ArrayList;
+import java.util.List;
+
+public class IOSimulator implements IO {
+
+    private List<String> inputs;
+    private int index;
+
+    private List<String> messaggi;
+
+    public IOSimulator(List<String> inputs) {
+        this.inputs = new ArrayList<>(inputs);
+        this.index = 0;
+        this.messaggi = new ArrayList<>();
+    }
+
+    public String leggiRiga() {
+        if (index >= inputs.size())
+            return null;
+        return inputs.get(index++);
+    }
+
+    public void mostraMessaggio(String messaggio) {
+        this.messaggi.add(messaggio);
+    }
+
+    public String getLastString() {
+        if (messaggi.isEmpty()) return null;
+        return messaggi.get(messaggi.size() - 1);
+    }
+
+    // utile per i test
+    public List<String> getMessaggi() {
+        return this.messaggi;
+    }
 }
